112cf8922073ab8c8fa8399bdbc6f8f2c305151e
mhunger <m.hunger@yahoo.de>
2017-08-29 20:57:26 +0200

    Add user class
    - added user class as required by backend for ride list
    - added id, email, password, first, last name , img, user rating and language

---
+++ web/carcada/src/app/ride-list/user.ts
@@ -0,0 +1,21 @@
+/**
+ * Created by michaelhunger on 09/07/17.
+ */
+
+export class User {
+  id: number;
+
+  email: string;
+
+  password: string;
+
+  firstname: string;
+
+  lastname: string;
+
+  img: string;
+
+  userRating: number;
+
+  language: string;
+}
