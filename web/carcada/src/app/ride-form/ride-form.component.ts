894a79af54826bb713bad4afe75b05bc63212728
mhunger <m.hunger@yahoo.de>
2017-08-29 20:56:10 +0200

    Add ride form empty component
    - added skeleton for ride form

---
+++ web/carcada/src/app/ride-form/ride-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ride-form',
+  templateUrl: './ride-form.component.html',
+  styleUrls: ['./ride-form.component.css']
+})
+export class RideFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
