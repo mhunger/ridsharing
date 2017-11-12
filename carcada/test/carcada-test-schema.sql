1cd1a09a79469875d38b8a5f14d68d45252ac7fb
mhunger <m.hunger@yahoo.de>
2017-11-12 20:56:36 +0100

    Add table fixtures for testing and test schema
    - added test data fixtures for rides and users
    - added schema that is loaded for each test run (db hast to exist)

---
+++ carcada/test/carcada-test-schema.sql
@@ -0,0 +1,94 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 4529
+#
+# http://www.sequelpro.com/
+# https://github.com/sequelpro/sequelpro
+#
+# Host: 127.0.0.1 (MySQL 5.5.55-0ubuntu0.14.04.1)
+# Database: carcada
+# Generation Time: 2017-11-12 17:12:10 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table message
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `message`;
+
+CREATE TABLE `message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `text` text,
+  `id_user_from` int(11) unsigned NOT NULL,
+  `id_user_to` int(11) unsigned NOT NULL,
+  `datetime` datetime NOT NULL,
+  `id_ride` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+# Dump of table ride
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `ride`;
+
+CREATE TABLE `ride` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `type` varchar(100) DEFAULT NULL,
+  `from` varchar(255) NOT NULL DEFAULT '',
+  `to` varchar(255) DEFAULT NULL,
+  `id_user_offered_by` int(11) unsigned DEFAULT NULL,
+  `travel_day` date DEFAULT NULL,
+  `departure_time` time DEFAULT NULL,
+  `arrival_time` time DEFAULT NULL,
+  `seats` int(11) unsigned NOT NULL,
+  `price` decimal(6,2) unsigned NOT NULL,
+  `departure_location` varchar(255) DEFAULT '',
+  `description` text,
+  `pet` tinyint(1) NOT NULL DEFAULT '0',
+  `twoseats` tinyint(1) DEFAULT '0',
+  `woman` tinyint(1) DEFAULT '0',
+  `smoker` tinyint(1) DEFAULT '0',
+  `baggage_size` varchar(255) DEFAULT '',
+  `detour` varchar(255) DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+# Dump of table user
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `user`;
+
+CREATE TABLE `user` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) CHARACTER SET latin1 NOT NULL DEFAULT '',
+  `password` varchar(255) CHARACTER SET latin1 DEFAULT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `first_name` varchar(255) DEFAULT NULL,
+  `img` varchar(255) DEFAULT NULL,
+  `user_rating` decimal(2,1) DEFAULT NULL,
+  `language` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
