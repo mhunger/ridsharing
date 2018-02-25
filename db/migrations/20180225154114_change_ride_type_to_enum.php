90395f18ddc1c807bda5ae51bdac0bd3cd6f3751
mhunger <m.hunger@yahoo.de>
2018-02-25 16:45:58 +0100

    Change ride type to enum
    - changed the ride type to enum "Privatfahrt", "Mietwagen"

---
+++ db/migrations/20180225154114_change_ride_type_to_enum.php
@@ -0,0 +1,34 @@
+<?php
+
+
+use Phinx\Migration\AbstractMigration;
+
+class ChangeRideTypeToEnum extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * http://docs.phinx.org/en/latest/migrations.html#the-abstractmigration-class
+     *
+     * The following commands can be used in this method and Phinx will
+     * automatically reverse them when rolling back:
+     *
+     *    createTable
+     *    renameTable
+     *    addColumn
+     *    renameColumn
+     *    addIndex
+     *    addForeignKey
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change()
+    {
+        $this->query('ALTER TABLE `ride` CHANGE `type` `type` ENUM(\'PRIVATFAHRT\',\'MIETAWAGEN\')  CHARACTER SET utf8  COLLATE utf8_general_ci  NOT NULL  DEFAULT \'PRIVATFAHRT\'');
+
+    }
+}
