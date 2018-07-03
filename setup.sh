dfcd28277418e98d9aea879a094890a936f81052
Roßgardt Benedikt <benedikt@rossgardt.de>
2018-07-03 23:58:45 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
 set -e
 
-# Todo: check if containers exists or if they are running
+########## DISCLAIMER: ##########
+######### Run only once #########
 
 # Start database
-# Todo: expose port to allow external connections
+# TODO: expose port to allow external connections
 docker run --name carcada_database -e MYSQL_ALLOW_EMPTY_PASSWORD=true -d mysql:5.7.22
 
 # Prepare backend image
-docker build -t carcada_backend .
+docker build -t carcada_app .
 
-# Launch backend and link databse
-docker run --link carcada_database:mysql -p7070:80 --name carcada_backend -d carcada_backend
-# Start nginx service
-docker exec -t carcada_backend nginx
+# Launch application container and link database
+docker run --link carcada_database:mysql -p7070:80 --name carcada_app -d carcada_app
 
-# Bootstrap database
-docker exec -t carcada_backend mysql -h carcada_database -e 'CREATE DATABASE carcada;'
-docker exec -t carcada_backend php backend/vendor/bin/phinx migrate -e development
-# Todo: run seeder
-# docker exec -t carcada_backend php vendor/bin/phinx seed:run
+# Bootstrap database by
+# 1) creating a database
+docker exec -t carcada_app mysql -h carcada_database -e 'CREATE DATABASE carcada;'
+# 2) scaffolding tables
+docker exec -t carcada_app php backend/vendor/bin/phinx migrate -e development
+# TODO: run seeder to create mock data
+# docker exec -t carcada_app php vendor/bin/phinx seed:run
 
