e9386d19311fba08050712d8cea38dde144edc96
Roßgardt Benedikt <benedikt@rossgardt.de>
2018-08-19 21:45:38 +0200

    Cleanup

--- frontend/src/app/state/ride-search/rides-search.effects.ts
+++ frontend/src/app/state/ride-search/rides-search.effects.ts
@@ -1,17 +1,14 @@
 import { Injectable } from "@angular/core";
 import { RidesSearchService } from '../../services/rides-search.service';
-import { Store } from "@ngrx/store";
 import { Effect, Actions } from "@ngrx/effects";
 import { RidesSearch, RidesSearchSuccess, RidesSearchFailure } from "./rides-search.actions";
 import { Observable } from 'rxjs';
 import { of as observableOf } from 'rxjs';
 import { switchMap, map, catchError } from 'rxjs/operators';
-import { RootState } from "../root.state";
 
 @Injectable()
 export class RidesSearchEffects {
   constructor(private actions$: Actions,
-              private store: Store<RootState>,
               private rideService: RidesSearchService) {
   }
 
