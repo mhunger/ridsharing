2c2f0d7c2ad62bf507f2a418b6728bbeb9a6f86f
Roßgardt Benedikt <benedikt@rossgardt.de>
2018-08-26 14:01:51 +0200

    Feature icons support multiple sizes

--- frontend/src/app/components/particles/feature-icon/feature-icon.component.ts
+++ frontend/src/app/components/particles/feature-icon/feature-icon.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { FeatureIconType } from './feature-icon.types';
 
+export enum IconSize {
+  Small= "small",
+  Medium= "medium",
+  Large= "large",
+}
+
 @Component({
   selector: 'feature-icon',
   templateUrl: 'feature-icon.component.html',
@@ -9,4 +15,5 @@ import { FeatureIconType } from './feature-icon.types';
 export class FeatureIconComponent {
   @Input() public type: FeatureIconType;
   @Input() public circle: boolean = true;
+  @Input() public size: IconSize = IconSize.Small;
 }
