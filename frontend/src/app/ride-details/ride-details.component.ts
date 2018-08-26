57c30f4e58b000da5522815d4f3d23062cba06eb
Roßgardt Benedikt <benedikt@rossgardt.de>
2018-08-26 19:00:25 +0200

    Try to retrieve ride data based on url parameter

--- frontend/src/app/ride-details/ride-details.component.ts
+++ frontend/src/app/ride-details/ride-details.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { Ride } from '../models/Ride';
 import { FeatureIconType } from '../components/particles/feature-icon/feature-icon.types';
 import { IconSize } from '../components/particles/feature-icon/feature-icon.component';
+import { ActivatedRoute } from '@angular/router';
+import { first, filter } from 'rxjs/operators';
+import { Store } from '@ngrx/store';
+import { RootState } from '../state/root.state';
 
 @Component({
   selector: 'app-ride-details',
@@ -14,9 +18,21 @@ export class RideDetailsComponent implements OnInit {
   public featureIconType = FeatureIconType;
   public largeIcon = IconSize.Large;
 
-  constructor() {
-    this.ride.id = 1;
-  }
+  constructor(private route: ActivatedRoute,
+              private store: Store<RootState>) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.route.params.pipe(first()).subscribe(params => {
+      const rideId = params.id;
+      this.store.select(state => state.ridesSearch.results)
+        .pipe(
+          filter(results => results !== null),
+          first(),
+        )
+        .subscribe(rides => {
+          if (!rides) return;
+          this.ride = rides.find(ride => ride.id === rideId);
+        });
+    });
+  }
 }
